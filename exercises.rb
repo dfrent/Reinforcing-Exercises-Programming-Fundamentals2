eb54a279171dfe66770d82564458efb8d9e2e5de
cordell <cordell.perry@gmail.com>
2017-11-14 09:38:35 -0500

    exercise 2 user_name access

--- exercises.rb
+++ exercises.rb
@@ -1,13 +1,27 @@
-the_numbers = [1, 2, 3, 6, 7, 8, 10, 11, 12, 13]
+# the_numbers = [1, 2, 3, 6, 7, 8, 10, 11, 12, 13]
+#
+# def odd_numbers(arry)
+#   total = 0
+#   arry.each do |number|
+#     if number % 2 == 1
+#       total += number
+#     end
+#   end
+#   return total
+# end
+#
+# puts odd_numbers(the_numbers)
 
-def odd_numbers(arry)
-  total = 0
-  arry.each do |number|
-    if number % 2 == 1
-      total += number
-    end
-  end
-  return total
+
+###------EXERCISE 2--------####
+
+the_names = ["Kevin", "Greg", "jesus"]
+
+puts "hello, whats your name"
+user_name = gets.chomp
+
+if the_names.detect {|name| name == user_name}
+    puts "hello #{user_name}"
+else
+  puts "who goes there?"
 end
-
-puts odd_numbers(the_numbers)
